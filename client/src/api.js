a2eada6cdbe4a560cbe6dd572083265bb1045abb
Ashish Jain <ashishfarnish135@gmail.com>
2026-01-22 10:25:09 +0530

    added credentials

--- client/src/api.js
+++ client/src/api.js
@@ -4,7 +4,7 @@ const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000/api";
 
 const api = axios.create({
   baseURL: API_BASE,
-  // withCredentials: true, // important: send/receive HttpOnly cookie
+  withCredentials: true, // important: send/receive HttpOnly cookie
 });
 
 export default api;
